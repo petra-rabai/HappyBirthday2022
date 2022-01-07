8ac0892b1e94601d85d839f8842f22d952beeb85
Petra Rábai <petra.rabai@icloud.com>
2022-01-07 13:22:15 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,72 +1,72 @@
 //Accordion handler start
-document.addEventListener("DOMContentLoaded", function(event) {
-    var acc = document.getElementsByClassName("accordion");
-    var panel = document.getElementsByClassName("panel");
-    var imageshow =document.getElementById("imageone");
-    for (var i = 0; i < acc.length; i++) {
-      acc[i].onclick = function() {
-        var setClasses = !this.classList.contains("active");
-        setClass(acc, "active", "remove");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-          panel.style.display = "none";
-          imageshow.style.display = "none"
-        } else {
-          panel.style.display = "block";
-          imageshow.style.display = "block";
-        }
-  
-        setClass(panel, "show", "remove");
-  
-        if (setClasses) {
-          this.classList.toggle("active");
-          this.nextElementSibling.classList.toggle("show");
-        }
-      };
-    }
-  
-    function setClass(els, className, fnName) {
-      for (var i = 0; i < els.length; i++) {
-        els[i].classList[fnName](className);
+document.addEventListener("DOMContentLoaded", function (event) {
+  var acc = document.getElementsByClassName("accordion");
+  var panel = document.getElementsByClassName("panel");
+  var imageshow = document.getElementById("imageone");
+  for (var i = 0; i < acc.length; i++) {
+    acc[i].onclick = function () {
+      var setClasses = !this.classList.contains("active");
+      setClass(acc, "active", "remove");
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+        imageshow.style.display = "none";
+      } else {
+        panel.style.display = "block";
+        imageshow.style.display = "block";
       }
-    }
-  });
-  // Accordion handler end
-  //Carousel handler start
-  var slideIndex = 1;
-  showSlidesdot(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlidesdot((slideIndex += n));
+
+      setClass(panel, "show", "remove");
+
+      if (setClasses) {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+      }
+    };
   }
-  
-  // Thumbnail image controls
-  function currentSlidedot(n) {
-    showSlidesdot((slideIndex = n));
+
+  function setClass(els, className, fnName) {
+    for (var i = 0; i < els.length; i++) {
+      els[i].classList[fnName](className);
+    }
   }
-  
-  function showSlidesdot(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    var imageshow =document.getElementById("imageone");
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    
-    imageshow.style.display = "none";
-    
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+});
+// Accordion handler end
+//Carousel handler start
+var slideIndex = 1;
+showSlidesdot(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlidesdot((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlidedot(n) {
+  showSlidesdot((slideIndex = n));
+}
+
+function showSlidesdot(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var imageshow = document.getElementById("imageone");
+  if (n > slides.length) {
+    slideIndex = 1;
   }
-  //Carousel handler end
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  imageshow.style.display = "none";
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+//Carousel handler end
